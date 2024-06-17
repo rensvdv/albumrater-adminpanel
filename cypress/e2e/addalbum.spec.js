1a2f35f2e68f05dfed24ac03e92d47132b61625e
Rens van der Velden <I491926@fhict.nl>
2024-06-17 14:15:10 +0200

    e2e update

--- cypress/e2e/addalbum.spec.js
+++ cypress/e2e/addalbum.spec.js
@@ -3,13 +3,14 @@ describe('Add Album Form', () => {
     cy.visit('/admin');
   });
 
-  it('should create an album with the standard form', () => {
+  it('should create an album with the standard form and show success message', () => {
     cy.get('input[data-cy=album-title]').type('New Album');
     cy.get('input[data-cy=album-artist]').type('New Artist');
     cy.get('form[data-cy=add-album-form]').submit();
 
     cy.contains('Album Aangemaakt').should('be.visible');
   });
+
 
   it('should show an error message on form submission failure', () => {
     cy.intercept('POST', 'http://localhost:8080/api/albums', {
